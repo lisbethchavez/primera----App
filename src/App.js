d024bea9f9456f8b10b7c17532609958581b0931
lisbethchavez <chavezlisbeth639@gmail.com>
2022-02-08 17:21:51 -0500

    Mostrar cursos- eliminar cursos-formulario nuevo curso

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+//import Contador from "./components/Contador";
+import Cursos from "./components/Cursos";
+//import Hola from "./components/Hola";
+
+const LISTA_CURSOS = [
+  {id:1, curso: 'Aprendiendo Laravel'},
+  {id:2, curso: 'Curso de Docker para principiantes'},
+  {id:3, curso: 'Construyendo REST API desde cero'},
+  {id:4, curso: 'Curso de POO'},
+];
+
 
 function App() {
+  const [cursos, setCursos] = useState(LISTA_CURSOS);
+
+  const eliminarCursos=(id)=> {
+    //console.log ("desde app - eliminado curso de id: ", id);
+    const nuevosCursos = cursos.filter( item => {
+      return item.id!==id;
+    });
+    //console.log(nuevosCursos);
+    setCursos(nuevosCursos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hola mundo REACT</h1> 
+{/*       <Hola mensaje="Este es un mensaje" pie="La fecha es 2022-feb-07"/>
+      <Hola mensaje="Esmeraldas" pie="Es una ciudad hermosa"/>
+      <Hola mensaje="Ecuador" pie="País Multicultural"/> */}
+      
+      {/* <Contador/> */}
+
+      <Cursos cursos={cursos} onEliminarCursos={eliminarCursos} />
+
     </div>
   );
 }
